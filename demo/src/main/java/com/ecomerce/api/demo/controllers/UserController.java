f5248b44824feef33059f8220d80ebeaed7e30a2
victoramayaferreira <victor.amaya.ferreira@gmail.com>
2024-03-26 14:43:57 -0500

    Adding user controller and service

---
+++ demo/src/main/java/com/ecomerce/api/demo/controllers/UserController.java
@@ -0,0 +1,10 @@
+package com.ecomerce.api.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+}
---
+++ demo/src/main/java/com/ecomerce/api/demo/services/UserService.java
@@ -0,0 +1,8 @@
+package com.ecomerce.api.demo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
